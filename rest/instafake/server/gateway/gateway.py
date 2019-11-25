7906a07a355945c9fe6e89fe2e8cbb54ba30534e
Pedro <pedrobeethoven8@gmail.com>
2019-11-25 20:54:01 -0300

    Fix gateway errors

--- rest/instafake/server/gateway/gateway.py
+++ rest/instafake/server/gateway/gateway.py
@@ -1,13 +1,18 @@
 from flask import Flask
 from flask import jsonify
+import json
 import requests
 
 app = Flask(__name__)
 
-@app.route("/teste")
-def hello():
-    response = requests.get("http://localhost:7777/users")
-    return jsonify(response.json())
+@app.route("/gw/users/<int:user_id>", methods=["GET"])
+def getUser(user_id):
+    response1 = requests.get("http://localhost:7777/users/"+str(user_id))
+    response2 = requests.get("http://localhost:8888/api/msi/images/"+str(user_id))
+    data1 = json.loads(response1.text)
+    data2 = json.loads(response2.text)
+    rConcat = data1 + data2
+    return jsonify(rConcat)
 
 
 if __name__ == '__main__':
