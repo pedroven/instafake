1c357f1219efd84aa6d0a5f3397b8d3b48b03b6e
Pedro <pedrobeethoven8@gmail.com>
2019-11-25 20:52:38 -0300

    Fix and Add new functions in ms-images

--- rest/instafake/server/ms-images/ms-images.go
+++ rest/instafake/server/ms-images/ms-images.go
@@ -14,15 +14,23 @@ import (
 )
 
 type Image struct {
-	ImgBase64 string `json:"image"`
+	ImgBase64 string `json:"imageData"`
 }
 
-type Images struct {
-	ImagesArray []string `json:"images"`
+type allImages []Image
+
+func reverse(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
 }
 
-func getImages(w http.ResponseWriter, r *http.Request) {
-	allImages := make([]string, 0)
+func getProfileImages(w http.ResponseWriter, r *http.Request) {
+	var images allImages
+	vars := mux.Vars(r)
+	key := vars["id"]
 	files, _ := ioutil.ReadDir("./source")
 	for _, f := range files {
 		imgFile, err := os.Open("./source/" + f.Name())
@@ -38,12 +46,46 @@ func getImages(w http.ResponseWriter, r *http.Request) {
 		fReader := bufio.NewReader(imgFile)
 		fReader.Read(buffer)
 
-		imgBase64Str := base64.StdEncoding.EncodeToString(buffer)
+		invStr := reverse(f.Name())
+		invStr = invStr[:6]
+		nStr := reverse(invStr)
+		fns := f.Name()
+		fNameStr := fns[:len(fns)-len(nStr)]
 
-		allImages = append(allImages, imgBase64Str)
+		if key == fNameStr {
+			var image Image
+			imgBase64Str := base64.StdEncoding.EncodeToString(buffer)
+			image.ImgBase64 = imgBase64Str
+
+			images = append(images, image)
+		}
 	}
-	var images Images
-	images.ImagesArray = allImages
+	json.NewEncoder(w).Encode(images)
+}
+
+func getImages(w http.ResponseWriter, r *http.Request) {
+	var images allImages
+	files, _ := ioutil.ReadDir("./source")
+	for _, f := range files {
+		imgFile, err := os.Open("./source/" + f.Name())
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		defer imgFile.Close()
+		fInfo, _ := imgFile.Stat()
+		size := fInfo.Size()
+		buffer := make([]byte, size)
+
+		fReader := bufio.NewReader(imgFile)
+		fReader.Read(buffer)
+
+		var image Image
+		imgBase64Str := base64.StdEncoding.EncodeToString(buffer)
+		image.ImgBase64 = imgBase64Str
+
+		images = append(images, image)
+	}
 	json.NewEncoder(w).Encode(images)
 }
 
@@ -78,7 +120,8 @@ func getOneImage(w http.ResponseWriter, r *http.Request) {
 func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.HandleFunc("/api/msi/images", getImages)
-	myRouter.HandleFunc("/api/msi/images/{id}", getOneImage)
+	myRouter.HandleFunc("/api/msi/image/{id}", getOneImage)
+	myRouter.HandleFunc("/api/msi/images/{id}", getProfileImages)
 	log.Fatal(http.ListenAndServe(":8888", myRouter))
 }
 
